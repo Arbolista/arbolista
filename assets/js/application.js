389867fd81d09bc378ff1f8b2e6ae4fb19a07acc
Eric Hulburd <eric@arbol.org>
2015-12-06 19:43:45 -0600

    mend

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,8 +2,8 @@
 //= require bootstrap/dist/js/bootstrap
 //= require angular/angular
 //= require angular-ui-router/release/angular-ui-router
-//= require d3/d3.min.js
+//= require d3/d3.min
 //= require bubbles
-//= require forge.min.js
+//= require forge.min
 //= require_tree ./angular/modules
 //= require_tree ./angular/lib
